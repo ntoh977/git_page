dca1155ebe3b83304f458e8b4e838fa72376f071
Anton <92940187+ntoh977@users.noreply.github.com>
2022-09-22 11:27:34 +0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+$('.single-item').slick({
+
+    arrows: true,
+    prevArrow:"<div class='visible-lg slick-prev'><img src='./img/Vector_4.png'></div>",
+    nextArrow:"<div class='visible-lg slick-next'><img src='./img/Vector_4.png' class='rotate'></div>",
+});
+});
