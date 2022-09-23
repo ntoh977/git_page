a49ffa9a74190b18efb36f8a4245f6d193958fe3
Anton <92940187+ntoh977@users.noreply.github.com>
2022-09-23 11:00:11 +0300

    new

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ $('.single-item').slick({
     prevArrow:"<div class='visible-lg slick-prev'><img src='./img/Vector_4.png'></div>",
     nextArrow:"<div class='visible-lg slick-next'><img src='./img/Vector_4.png' class='rotate'></div>",
 });
+
+
+$('.slider__one').slick({
+    cssEase: 'linear',
+    arrows: true,
+    prevArrow: "<div class='visible-lg slick-prev'><img src='./img/Vector_4.png'></div>",
+    nextArrow: "<div class='visible-lg slick-next'><img src='./img/Vector_4.png' class='rotate'></div>",
+});
 });
